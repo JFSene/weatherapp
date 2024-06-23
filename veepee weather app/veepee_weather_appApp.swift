e4b89d388b194314e68237c9b2e0817f5347a70e
JFSene <joel.sene@hotmail.com>
2024-06-23 13:39:44 +0100

    Initial Commit

---
+++ veepee weather app/veepee_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  veepee_weather_appApp.swift
+//  veepee weather app
+//
+//  Created by Joel Sene on 23/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct veepee_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
